8a5742cadf4332b9419cd5d4e4ba4adf03984c49
r68720282-afk <r68720282@gmail.com>
2026-07-26 11:57:17 +0530

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
 
 import HeroSection from "../components/HeroSection";
 import WhyChooseUs from "../components/WhyChooseUs";
@@ -6,17 +7,13 @@ import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
 import ProcessSection from "../components/ProcessSection";
 import GallerySection from "../components/GallerySection";
-
-
 import HealthcareQueryForm from "../components/HealthcareQueryForm";
 
 export default function HomePage() {
   return (
     <>
       <Helmet>
-        <title>
-          Prakrati Healthcare & Education
-        </title>
+        <title>Prakrati Healthcare & Education</title>
 
         <meta
           name="description"
@@ -24,10 +21,20 @@ export default function HomePage() {
         />
       </Helmet>
 
+      {/* Temporary Admin Login Button */}
+      <div className="fixed top-24 right-6 z-50">
+        <Link
+          to="/admin-login"
+          className="bg-blue-600 hover:bg-blue-700 text-white px-5 py-3 rounded-xl shadow-lg font-semibold"
+        >
+          Admin Login
+        </Link>
+      </div>
+
       <HeroSection />
 
       <WhyChooseUs />
-      
+
       <AboutSection />
 
       <ServicesSection />
