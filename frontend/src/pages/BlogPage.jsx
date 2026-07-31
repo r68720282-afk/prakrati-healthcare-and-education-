252cbd152e934f6f0cb3b354252bc01f0fe16d23
r68720282-afk <r68720282@gmail.com>
2026-07-31 21:19:20 +0530

    Update BlogPage.jsx

--- frontend/src/pages/BlogPage.jsx
+++ frontend/src/pages/BlogPage.jsx
@@ -1,1 +1,68 @@
-export default function BlogPage(){return <div className='max-w-6xl mx-auto p-6'><h1 className='text-3xl font-bold'>Blog</h1><p className='mt-3'>Blog content for Prakarati Healthcare & Education.</p></div>}
+import { Routes, Route } from "react-router-dom";
+
+import Layout from "./components/Layout";
+import Chatbot from "./components/Chatbot";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import CareersPage from "./pages/CareersPage";
+import BlogPage from "./pages/BlogPage";
+// Agar BlogDetailsPage bani hai to niche wali line uncomment kar dena
+// import BlogDetailsPage from "./pages/BlogDetailsPage";
+
+import AdminPage from "./pages/AdminPage";
+import AdminLoginPage from "./pages/AdminLoginPage";
+import HealthcareQueriesPage from "./pages/admin/HealthcareQueriesPage";
+
+export default function App() {
+  return (
+    <Layout>
+      <Routes>
+        {/* Public Pages */}
+
+        <Route path="/" element={<HomePage />} />
+
+        <Route path="/about" element={<AboutPage />} />
+
+        <Route path="/blog" element={<BlogPage />} />
+
+        {/* Agar BlogDetailsPage bani hai to ye bhi add karna */}
+        {/* <Route path="/blog/:slug" element={<BlogDetailsPage />} /> */}
+
+        <Route path="/careers" element={<CareersPage />} />
+
+        {/* Admin Login */}
+
+        <Route
+          path="/admin-login"
+          element={<AdminLoginPage />}
+        />
+
+        {/* Admin Dashboard */}
+
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <AdminPage />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Healthcare Queries */}
+
+        <Route
+          path="/admin/queries"
+          element={
+            <ProtectedRoute>
+              <HealthcareQueriesPage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+
+      <Chatbot />
+    </Layout>
+  );
+}
