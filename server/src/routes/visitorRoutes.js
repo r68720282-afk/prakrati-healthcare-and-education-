5ab68dd0ab3ae9c13b6f2ae511e71233e0b11864
r68720282-afk <r68720282@gmail.com>
2026-09-05 16:53:03 +0530

    Update visitorRoutes.js

--- server/src/routes/visitorRoutes.js
+++ server/src/routes/visitorRoutes.js
@@ -3,14 +3,13 @@ import {
   trackVisitor,
   getVisitorStats,
 } from "../controllers/visitorController.js";
-router.get("/stats", getVisitorStats);
 
 const router = express.Router();
 
-// Public: Har visitor ko track karega
+// Public - har visitor ko track karega
 router.post("/track", trackVisitor);
 
-// Admin: Visitor statistics dekhega
-router.get("/stats", protectAdmin, getVisitorStats);
+// Public - total visitor count return karega
+router.get("/stats", getVisitorStats);
 
 export default router;
